a0a3ba2fbd23dc4d4323266c6b49246ec0acb129
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-31 17:49:18 -0400

    Saving data in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,6 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article)permit(:title, :text)
+    params.require(:article).permit(:title, :text)
   end
 end
