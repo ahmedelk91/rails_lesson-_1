f20f6ea38686f41b9b6b9fbe9bea95c46b5eddfa
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-31 21:24:18 -0400

    added comments and deleting comments/completed the homework.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,12 @@ def create
   @article = Article.find(params[:article_id])
   @comment = @article.comments.create(comment_params) redirect_to article_path(@article)
 end
+
+def destroy
+  @article = Article.find(params[:article_id])
+  @comment = @article.comments.find(params[:id])
+  @comment.destroy
+  redirect_to article_path(@article)
 end
 
 private
